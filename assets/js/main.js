dc35e7fad0bb7840732d1b3714406d9337627264
Vagner Treguas <vagner.treguas@gmail.com>
2021-02-05 15:31:04 -0300

    finalizando paginas

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,94 +3,69 @@ let btnInput = document.querySelector('.btn-input');
 let btnPesquisar = document.querySelector('.btn-primary');
 var content = document.getElementById('div-main');
 
+const xhr1 = new XMLHttpRequest();
 
 btnPesquisar.addEventListener('click', ()=> {
 
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=+${selectOption.value}:${btnInput.value}&key=AIzaSyByRTlliIKCn19yuOCQOdRZUjhz1gGYBa4`)
-    .then((response)=> {
-        response.json().then((info)=> {
 
-            
+  xhr1.open("GET",
+        `https://www.googleapis.com/books/v1/volumes?q=+${selectOption}:${btnInput}&key=AIzaSyByRTlliIKCn19yuOCQOdRZUjhz1gGYBa4`
+    );
 
-            
+    xhr1.onreadystatechange = function () {
+        if (xhr1.status = 200 && xhr1.readyState == 4) {
 
-            for(let i=0; i<= info.items.length; i++) { 
+            let dadosJSONText = xhr1.responseText
+            let info = JSON.parse(dadosJSONText)
 
-                content.innertHTML += `<div class="col">
-                <div class="card shadow-sm">
-                <img id="thumbnail" src="${info.items[i].volumeInfo.imageLinks.thumbnail}">
-                
-                <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">API do Livro</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Mais Informações</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Link de compra</button>
-      
-                      </div>
+            for (let i = 0; i <= info.items.length; i++) {
+                content.innerHTML +=
+                    `<div class="col">
+                    <div class="card shadow-sm">
+                    <img id="thumbnail" src="${info.items[i].volumeInfo.imageLinks.thumbnail}" style="width:200px;">
+                    <div class="card-body">
+                    <p id="title" class="card-text">
+                        Titulo: ${info.items[i].volumeInfo.title}
+                    </p>
+                    <p id="subt" class="card-text">
+                        Subtitulo: ${info.items[i].volumeInfo.subtitle}
+                    </p>
+                    <p>
+                        Categoria: ${info.items[i].volumeInfo.categories}
+                    </p>
+                    <p id="autor" class="card-text">
+                        Autor: ${info.items[i].volumeInfo.authors}
+                    </p>
+                    <p id="date" class="card-text">
+                        Data de Publicação: ${info.items[i].volumeInfo.publishedDate}
+                    </p>
+                    <p id="desc" class="card-text">
+                        Descrição: ${info.items[i].volumeInfo.description}
+                    </p>
                     </div>
-                  </div>
-                </div>
-              </div>
-              `
+                    <div id="moreinfo">
+                    <a id="apilivro" target="_blank" class="link" href="${info.items[i].selfLink}">
+                        API do Livro
+                    </a>
+                    <a href="${info.items[i].volumeInfo.infoLink}" class="link" target="_blank">
+                        Mais Informações
+                    </a>
+                    <a href="${info.items[i].saleInfo.buyLink}" class="link" target="_blank">
+                        Link de Compra
+                    </a>
+                    </div>
+                    </>`
             }
-
-        })
-    })
-
-//     const xhr1 = new XMLHttpRequest();
-
-
-
-//     xhr1.open("GET",
-//     `https://www.googleapis.com/books/v1/volumes?q=+${selectOption.value}:${btnInput.value}&key=AIzaSyByRTlliIKCn19yuOCQOdRZUjhz1gGYBa4`
-// );
-
-// xhr1.onreadystatechange = function () {
-//     if (xhr1.status = 200 && xhr1.readyState == 4) {
-
-//         let dadosJSONText = xhr1.responseText
-//         let info = JSON.parse(dadosJSONText)
-
-//         for (let i = 0; i <= info.items.length; i++) {
-//             content.innerHTML +=
-//                 `<div class="demo-card-wide mdl-card mdl-shadow--2dp" align="center" style="width:49%; justify-content:center; align-items:center">
-//                 <img id="thumbnail" src="${info.items[i].volumeInfo.imageLinks.thumbnail}" style="width:200px;">
-//                 <p id="title" class="hi">
-//                     Titulo: ${info.items[i].volumeInfo.title}
-//                 </p>
-//                 <p id="subt" class="hi">
-//                     Subtitulo: ${info.items[i].volumeInfo.subtitle}
-//                 </p>
-//                 <p>
-//                     Categoria: ${info.items[i].volumeInfo.categories}
-//                 </p>
-//                 <p id="autor" class="hi">
-//                     Autor: ${info.items[i].volumeInfo.authors}
-//                 </p>
-//                 <p id="date" class="hi">
-//                     Data de Publicação: ${info.items[i].volumeInfo.publishedDate}
-//                 </p>
-//                 <p id="desc" style="text-align:justify">
-//                     Descrição: ${info.items[i].volumeInfo.description}
-//                 </p>
-//                 <div id="moreinfo">
-//                 <a id="apilivro" target="_blank" class="link" href="${info.items[i].selfLink}">
-//                     API do Livro
-//                 </a>
-//                 <a href="${info.items[i].volumeInfo.infoLink}" class="link" target="_blank">
-//                     Mais Informações
-//                 </a>
-//                 <a href="${info.items[i].saleInfo.buyLink}" class="link" target="_blank">
-//                     Link de Compra
-//                 </a>
-//                 </div>
-//                 </>`
-//         }
-//     }
-// }
-// xhr1.send();
-
+        }
+    }
+    xhr1.send();
 })
+
+
+document.getElementById("Input").addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("srcbtn").click();
+  }
+});
 
